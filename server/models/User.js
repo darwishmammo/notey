363e1067e2ab087dbbc54c403967aa67a429794a
Darwish Mammo <darwish.mammo@gmail.com>
2020-10-09 09:58:48 +0200

    Add validation to User model

--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,61 @@
 const usersCollection = require("../dbConnection").db().collection("users");
+const validator = require("validator");
 
 let User = function (data) {
   this.data = data;
   this.errors = [];
+};
+
+User.prototype.cleanUp = function () {
+  if (typeof this.data.username != "string") {
+    this.data.username = "";
+  }
+  if (typeof this.data.email != "string") {
+    this.data.email = "";
+  }
+  if (typeof this.data.password != "string") {
+    this.data.password = "";
+  }
+
+  this.data = {
+    username: this.data.username.trim().toLowerCase(),
+    email: this.data.email.trim().toLowerCase(),
+    password: this.data.password,
+  };
+};
+
+User.prototype.validate = function () {
+  return new Promise(async (resolve, reject) => {
+    if (this.data.username == "") {
+      this.errors.push("You must provide a username.");
+    }
+    if (
+      this.data.username != "" &&
+      !validator.isAlphanumeric(this.data.username)
+    ) {
+      this.errors.push("Username can only contain letters and numbers.");
+    }
+    if (!validator.isEmail(this.data.email)) {
+      this.errors.push("You must provide a valid email address.");
+    }
+    if (this.data.password == "") {
+      this.errors.push("You must provide a password.");
+    }
+    if (this.data.password.length > 0 && this.data.password.length < 12) {
+      this.errors.push("Password must be at least 12 characters.");
+    }
+    if (this.data.password.length > 50) {
+      this.errors.push("Password cannot exceed 50 characters.");
+    }
+    if (this.data.username.length > 0 && this.data.username.length < 3) {
+      this.errors.push("Username must be at least 3 characters.");
+    }
+    if (this.data.username.length > 30) {
+      this.errors.push("Username cannot exceed 30 characters.");
+    }
+
+    resolve();
+  });
 };
 
 User.prototype.register = function () {
