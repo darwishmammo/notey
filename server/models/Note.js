2a333e32631a789343ef5de6a91bf80ef73a63d8
Darwish Mammo <darwish.mammo@gmail.com>
2020-10-13 14:22:28 +0200

    Add validation to note model

--- server/models/Note.js
+++ server/models/Note.js
@@ -7,17 +7,45 @@ let Note = function (data, userId) {
   this.userId = userId;
 };
 
+Post.prototype.cleanUp = function () {
+  if (typeof this.data.title != "string") {
+    this.data.title = "";
+  }
+  if (typeof this.data.body != "string") {
+    this.data.body = "";
+  }
+
+  this.data = {
+    createdDate: new Date(),
+  };
+};
+
+Post.prototype.validate = function () {
+  if (this.data.title == "") {
+    this.errors.push("A note must have a title.");
+  }
+  if (this.data.body == "") {
+    this.errors.push("A note must have a body.");
+  }
+};
+
 Note.prototype.create = function () {
   return new Promise((resolve, reject) => {
-    notesCollection
-      .insertOne(this.data)
-      .then((inserted) => {
-        resolve(inserted);
-      })
-      .catch((e) => {
-        this.errors.push("Error! Try again later.");
-        reject(this.errors);
-      });
+    this.cleanUp();
+    this.validate();
+    if (!this.errors.length) {
+      notesCollection
+        .insertOne(this.data)
+        .then((inserted) => {
+          resolve(inserted);
+        })
+        .catch((e) => {
+          this.errors.push("Error! Try again later.");
+          reject(this.errors);
+        });
+    } else {
+      reject(this.errors);
+    }
   });
 };
 
