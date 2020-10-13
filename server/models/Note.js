cb31d3e344e70b13f9c7ee70c7b1666aa5597f42
Darwish Mammo <darwish.mammo@gmail.com>
2020-10-13 13:49:33 +0200

    Add create function to note model

--- server/models/Note.js
+++ server/models/Note.js
@@ -7,4 +7,18 @@ let Note = function (data, userId) {
   this.userId = userId;
 };
 
+Post.prototype.create = function () {
+  return new Promise((resolve, reject) => {
+    notesCollection
+      .insertOne(this.data)
+      .then((inserted) => {
+        resolve(inserted);
+      })
+      .catch((e) => {
+        this.errors.push("Error! Try again later.");
+        reject(this.errors);
+      });
+  });
+};
+
 module.exports = Note;
